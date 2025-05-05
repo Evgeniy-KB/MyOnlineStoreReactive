7c37bcb08a3d34171aa318c5deaf966141fbcc17
Evgeniy <132995815+Evgeniy-KB@users.noreply.github.com>
2025-05-05 18:58:52 +0500

    init commit

---
+++ src/main/java/task6/MyOnlineStoreReactive/MyOnlineStoreReactiveApplication.java
@@ -0,0 +1,13 @@
+package task6.MyOnlineStoreReactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOnlineStoreReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOnlineStoreReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/task6/MyOnlineStoreReactive/MyOnlineStoreReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package task6.MyOnlineStoreReactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOnlineStoreReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
